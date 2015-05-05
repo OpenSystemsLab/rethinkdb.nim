f09e815dab69be000e441c5aac8cedcbf71bdda0
Bruce Doan <rgv151@gmail.com>
2015-05-06 02:11:56 +0700

    Parse responseHeader from server

--- rethinkdb.nim
+++ rethinkdb.nim
@@ -1,10 +1,12 @@
 import asyncdispatch
 import rawsockets
-import posix
 import endians
 import strutils
+import logging
+import parseutils
 
 import ql2
+import utils
 
 const
   BUFFER_SIZE: int = 1024
@@ -16,7 +18,7 @@ type
     port: Port
     auth: string
     sock: TAsyncFD
-    queryToken: int32
+    queryToken: uint64
 
   RqlDriverError* = object of SystemError
   RqlIOError* = object of IOError
@@ -24,16 +26,18 @@ type
 
   RethinkClient* = ref RethinkClientBase
 
+var
+  L = newConsoleLogger()
 
 when defined(windows):
   import winlean
 
-  # TODO implemen `sendPacket` proc
+  # TODO implemen `sendInt` proc
 else:
   import posix
 
-  proc sendPacket*(r: RethinkClient, data: int): Future[void] =
-    var future = newFuture[void]("sendPacket")
+  proc sendInt*(r: RethinkClient, data: int): Future[void] =
+    var future = newFuture[void]("sendInt")
 
     proc cb(sock: TAsyncFD): bool =
       result = true
@@ -49,6 +53,39 @@ else:
     addWrite(r.sock, cb)
     return future
 
+  proc sendQueryToken(r: RethinkClient) =
+    var buf: int64
+    r.queryToken.inc()
+    bigEndian64(addr buf, addr r.queryToken)
+    discard r.sock.SocketHandle.send(addr buf, sizeof(buf), MSG_NOSIGNAL)
+
+
+proc send*(r: RethinkClient, data: string): Future[void] =
+  result = r.sock.send(data)
+
+proc sendQuery*(r: RethinkClient, query: string): Future[void] =
+  L.log(lvlDebug, "Sending query: $#" % [query])
+  var future = newFuture[void]("sendQuery")
+  proc cb(sock: TAsyncFD): bool =
+    result = true
+
+    #r.sendQueryToken()
+    #discard r.sendInt(query.len)
+    var buf: int64
+    r.queryToken.inc()
+    bigEndian64(addr buf, addr r.queryToken)
+    discard r.sock.SocketHandle.send(addr buf, sizeof(buf), MSG_NOSIGNAL)
+    var query = query
+    var queryLen = query.len
+    var buf1: int32
+    littleEndian32(addr buf1, addr queryLen)
+    discard r.sock.SocketHandle.send(addr buf1, sizeof(buf1), MSG_NOSIGNAL)
+    discard r.sock.SocketHandle.send(addr query, query.len, MSG_NOSIGNAL)
+
+    future.complete()
+
+  addWrite(r.sock, cb)
+  return future
 
 proc newRethinkClient*(address: string, port = Port(28015), auth = ""): RethinkClient =
 
@@ -60,37 +97,44 @@ proc newRethinkClient*(address: string, port = Port(28015), auth = ""): RethinkC
   result.port = port
   result.auth = auth
   result.sock = newAsyncRawSocket()
-
-
-proc send*(r: RethinkClient, data: string): Future[void] =
-  result = r.sock.send(data)
-
+  result.queryToken = 0
 
 proc handshake*(r: RethinkClient) {.async.} =
-  await r.sendPacket(HandshakeV0_4)
-  await  r.sendPacket(r.auth.len)
+  L.log(lvlDebug, "Preparing handshake...")
+  await r.sendInt(HandshakeV0_4)
+  await  r.sendInt(r.auth.len)
   if r.auth.len > 0:
     await r.send(r.auth)
-  await r.sendPacket(HandshakeJSON)
+  await r.sendInt(HandshakeJSON)
 
-  var data = await r.sock.recv(1024)
+  var data = await r.sock.recv(BUFFER_SIZE)
   if not data.startsWith("SUCCESS"):
     raise newException(RqlDriverError, data)
+  L.log(lvlDebug, "Handshake success...")
 
 proc connect*(r: RethinkClient) {.async.} =
+  L.log(lvlDebug, "Connecting to server at $#:$#..." % [r.address, $r.port])
   await r.sock.connect(r.address, r.port)
   await r.handshake()
 
 proc processResponse(r: RethinkClient) {.async.} =
   while true:
-    var data = await r.sock.recv(BUFFER_SIZE)
-    if data.len > 0:
-      echo "processResponse: ", data
+    var data = await r.sock.recv(8)
+    var token = data.toInt
+    data = await r.sock.recv(4)
+    var lenLe = data.toInt32
+    var len: int
+    bigEndian32(addr len, addr lenLe)
+    var message = await r.sock.recv(len)
+    echo "[$#, $#, $#]" % [$token, $len, message]
 
 
 proc run*(r: RethinkClient) {.async.} =
   await r.connect()
   asyncCheck r.processResponse()
+  await r.sendQuery("foo")
+  await r.sendQuery("blah")
+
 
 
 when isMainModule:
