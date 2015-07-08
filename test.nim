144f93ad5477c6f705c509780697e530feb2406a
Bruce Doan <rgv151@gmail.com>
2015-07-09 02:15:50 +0700

    update test

--- test.nim
+++ test.nim
@@ -1,12 +1,11 @@
 import asyncdispatch
-
 import rethinkdb
 
 
 proc main() {.async.} =  
   var r = newRethinkClient()
-  await r.db("test").table("posts").run()
-    #.filter({"name": newStringDatum("Hello World!")}).run()
+  discard await r.db("test").table("users").filter({"username": newStringDatum("admin"), "active": newBoolDatum(true)}).run()
+  r.disconnect()
 
 when isMainModule:
   asyncCheck main()
