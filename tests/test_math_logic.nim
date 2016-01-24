9a674a847096d26a58c244c85ea81606e96fa933
Bruce Doan <rgv151@gmail.com>
2016-01-25 00:14:42 +0700

    add more tests for `add`

--- tests/test_math_logic.nim
+++ tests/test_math_logic.nim
@@ -10,10 +10,31 @@ suite "math and logic tests":
     let output = 2
     var ret: JsonNode
     ret = (r.expr(1) + 1).run()
-    check(ret.getNum() == output)
+    check(ret.num == output)
     ret = (1 + r.expr(1)).run()
-    check(ret.getNum() == output)
+    check(ret.num == output)
     ret = r.expr(1).add(1).run()
-    check(ret.getNum() == output)
+    check(ret.num == output)
+
+    ret = (r.expr(1.75) + 8.5).run()
+    check(ret.fnum == 10.25)
+
+  test "add w/ strings":
+    var ret = (r.expr("") + "").run()
+    check(ret.str == "")
+
+    ret = (r.expr("abc") + "def").run()
+    check(ret.str == "abcdef")
+
+  test "add w/ arrays":
+    var ret = (r.expr([1,2]) + [3] + [4,5] + [6,7,8]).run()
+    check(ret == %*[1,2,3,4,5,6,7,8])
+
+  test "type errors":
+    expect(RqlRuntimeError):
+      discard (r.expr(1) + "a").run()
+
+    expect(RqlRuntimeError):
+      discard (r.expr("a") + 1).run()
 
 r.close()
